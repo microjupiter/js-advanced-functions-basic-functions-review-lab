4e7ba8cf85cfc1024f92d0f790200da8ce38a270
Kyle Wilson <kyle@hinterlandstills.com>
2021-03-05 09:38:40 -0600

    commit tree 333b9066acd4cc22d2c16832d66df009ebe296c8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun()
+
+function mondayWork(work="go to the office"){
+  return (`This Monday, I will ${work}.`)
+}
+mondayWork()
+
+function wrapAdjective(flair="*"){
+  return function(adjective="special") {
+    return `You are ${flair}${adjective}${flair}!`
+  } 
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b
+  }
+}
